1a0cf0f280bc75e4a2d8c44ee8f6c306f6c5f56b
MukeshTripathy123 <117341431+MukeshTripathy123@users.noreply.github.com>
2022-11-05 10:16:29 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,29 @@
+package javabasicprogram;
+import java.util.Scanner;
+public class FactorialNumber {
+	
+public static void main(String[]args) {	
+			
+int n, fact=1;	
+System.out.println("Enter any number");	
+	Scanner sc=new Scanner(System.in);	
+		
+
+ n=sc.nextInt();
+ 
+for(int i=1; i<=n;i++)
+
+{
+fact=fact*i;
+
+}
+
+System.out.println("factorial" +fact);
+}
+}
+	
+
+
+
+
+
